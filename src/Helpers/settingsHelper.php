ca6a17064ca83405f29acddbde8fee88293a1b99
Damodar Bhattarai <damodar.bhattarai.1999@gmail.com>
2023-01-27 20:18:17 +0545

    added short text also

--- src/Helpers/settingsHelper.php
+++ src/Helpers/settingsHelper.php
@@ -11,3 +11,18 @@ if (!function_exists('settings')) {
     }
 }
 
+
+if (!function_exists('short_text')) {
+    function short_text($text, $length = 30, $read_more = false, $link = false, $style = '')
+    {
+        if ($read_more && $link) {
+            $moreTag = '...<a ' . $style . ' href="' . $link . '">Read More</a>';
+        } else {
+            $moreTag = '...';
+        }
+        return Illuminate\Support\Str::limit(strip_tags(html_entity_decode($text)), $length, $moreTag);
+    }
+}
+
+
+
